8b4029217e8a244d573576b858b047c685766bf3
재연 <charmant1402@gmail.com>
2022-02-18 22:40:43 +0900

    삭제

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Auth from "./components/Auth";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Auth />
-      </header>
+      <Auth />
     </div>
   );
 }
