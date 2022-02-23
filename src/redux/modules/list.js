fb001e65bd21ae59c19970786bb50929d399d8a6
durukim <kimduru@naver.com>
2022-02-23 12:41:50 +0900

    리더 박

--- src/redux/modules/list.js
+++ src/redux/modules/list.js
@@ -5,12 +5,14 @@ import axios from "axios";
 import { history } from "../../App";
 
 const GET_LIST = "GET_LIST";
+const GET_LIST_DETAIL = "GET_LIST_DETAIL";
 
 const initialState = {
   list: [],
 };
 
 const getList = createAction(GET_LIST, (list) => ({ list }));
+const getListDetail = createAction(GET_LIST_DETAIL, (listOne) => ({ listOne }));
 
 const getListDB = (listId) => {
   return function (dispatch, getState, { history }) {
@@ -26,18 +28,40 @@ const getListDB = (listId) => {
   };
 };
 
+const getListDetailDB = (placeId) => {
+  return function (dispatch, getState, { history }) {
+    apis
+      .getDetail(placeId)
+      .then((res) => {
+        console.log(res);
+        dispatch(getListDetail(res.data.place));
+      })
+      .catch((err) => {
+        console.log("nooo");
+        console.log(err);
+      });
+  };
+};
+
 export default handleActions(
   {
     [GET_LIST]: (state, action) =>
       produce(state, (draft) => {
         draft.list = action.payload.list;
       }),
+    [GET_LIST_DETAIL]: (state, action) =>
+      produce(state, (draft) => {
+        draft.listOne = action.payload.listOne;
+      }),
   },
   initialState
 );
 
 const actionCreators = {
+  getList,
   getListDB,
+  getListDetail,
+  getListDetailDB,
 };
 
 export { actionCreators };
